7fb9f74620b5a377234554fb5fbead246c3571fc
Rod20 <nimrod2560@gmail.com>
2020-06-09 18:40:09 -0400

    Modificando la paleta de colores

--- lib/ui/resources/app_colors.dart
+++ lib/ui/resources/app_colors.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 
 const Color lightPrimary      = Colors.white;
 const Color darkPrimary       = Colors.black;
-const Color lightAccent       = Color(0xffC68354);
-const Color darkAccent        = Color(0xff211717);
+const Color lightAccent       = Color(0xff3f52ba);
+const Color darkAccent        = Color(0xff1a2d66);
 const Color lightBG           = Colors.white;
-const Color darkBG            = Colors.black54;
-const Color badgeColor        = Colors.red;
-const Color buttonColor       = Color(0xffC68354);
+const Color darkBG            = Color(0xff070709);
+const Color badgeColor        = Color(0xffd54b76);
+const Color secondBadgeColor  = Color(0xFF773c60);
+const Color buttonColor       = Color(0xff26c7c3);
 const Color transparentColor  = Colors.transparent;
-const Color opacityTitleColor = Color(0x80ffffff);
-const Color greyBG            = Color(0xFFFAFAFA);
-const Color lightGreyColor   = Color(0xFFBEBEBE);
+const Color opacityTitleColor = Color(0x80657291);
+const Color greyBG            = Color(0xFFbbb6c2);
+const Color lightGreyColor    = Color(0xFFBEBEBE);
 
 // Acá Ale tiene que ir lo que utilices con respecto a colores la parte de arriba es la general,
 // si necesitas algo en personal para una sola cosa, lo pones debajo de esto
